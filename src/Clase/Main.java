90d3b8525ee8d978d266e36c63f35d252c90332c
Edgeworth <Edgeworth@Edgeworth>
2023-01-28 01:14:55 -0500

    version 1.0

--- src/Clase/Main.java
+++ src/Clase/Main.java
@@ -5,7 +5,7 @@ package Clase;
 public class Main {
     public static void main(String[] args) {
         ClasePadre mensaje=new Consulta();
-        mensaje.setSaldo(100000);
+        mensaje.setSaldo(1000000);
         mensaje.Operaciones();
     }
 }
